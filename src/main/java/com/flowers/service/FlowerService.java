b7579bb9d8ecb5fb598f1245f2243571aa328289
katekiselevich <kiselkath@gmail.com>
2025-06-19 17:42:08 +0200

    Added FlowerService

--- src/main/java/com/flowers/service/FlowerService.java
+++ src/main/java/com/flowers/service/FlowerService.java
@@ -1,10 +1,28 @@
 package com.flowers.service;
 
+import com.flowers.collector.UniqueCareActionCollector;
+import com.flowers.model.Flower;
 import com.flowers.model.Gardener;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class FlowerService {
-    public List<String> flattenCareActions(List<Gardener> gardeners) { /* TODO */ }
-    public Map<String, Long> countFlowersByType(List<Gardener> gardeners) { /* TODO */ }
-    public Set<String> collectUniqueCareActions(List<Gardener> gardeners) { /* TODO */ }
+    public List<String> flattenCareActions(List<Gardener> gardeners) {
+        return gardeners.stream()
+                .flatMap(g -> g.getAssignedFlowers().stream())
+                .flatMap(f -> f.getCareActions().stream())
+                .collect(Collectors.toList());
+    }
+    public Map<String, Long> countFlowersByType(List<Gardener> gardeners) {
+        return gardeners.stream()
+                .flatMap(g -> g.getAssignedFlowers().stream())
+                .collect(Collectors.groupingBy(
+                        Flower::getName,
+                        Collectors.counting()
+                ));
+    }
+    public Set<String> collectUniqueCareActions(List<Gardener> gardeners) {
+        return gardeners.stream()
+                .collect(new UniqueCareActionCollector());
+    }
 }
